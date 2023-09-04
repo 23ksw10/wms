bb05660e5898576dacac815de1deb4eee2a36173
Kim sun wook <kimsw9603@gmail.com>
2023-09-05 02:17:10 +0900

    Initial commit

---
+++ src/test/java/com/sunwook/wms/WmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunwook.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
