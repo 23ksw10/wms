8a3710384a7f022cf7ad0225b622a1218069db89
Kim sun wook <kimsw9603@gmail.com>
2023-09-05 16:44:32 +0900

    2. create test case RegisterProductTest

--- src/test/java/com/sunwook/wms/product/feature/RegisterProductTest.java
+++ src/test/java/com/sunwook/wms/product/feature/RegisterProductTest.java
@@ -1,4 +1,51 @@
 package com.sunwook.wms.product.feature;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterProductTest {
+
+    private RegisterProduct registerProduct;
+
+    @BeforeEach
+    void setUp() {
+        registerProduct = new RegisterProduct;
+    }
+
+    @Test
+    @DisplayName("상품을 생성한다.")
+    void registerProduct() {
+        RegisterProduct.Request request = new RegisterProduct.Request(
+                "name",
+                "code",
+                "description",
+                "brand",
+                "maker",
+                "origin",
+                Category.ELECTRONICS,
+
+                );
+    }
+
+    public enum Category {
+
+        ELECTRONICS("전자기기");
+
+        private final String description;
+
+        Category(final String description) {
+            this.description = description;
+        }
+    }
+
+    public static class RegisterProduct {
+        public void request() {
+
+        }
+
+        public record Request() {
+
+        }
+    }
 }
