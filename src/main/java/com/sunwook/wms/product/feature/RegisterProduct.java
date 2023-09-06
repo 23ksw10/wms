6a0b0380f6f33eec03523a042a1c58cfd056f8c7
Kim sun wook <kimsw9603@gmail.com>
2023-09-07 02:22:10 +0900

    8. RegisterProduct API로 변경

--- src/main/java/com/sunwook/wms/product/feature/RegisterProduct.java
+++ src/main/java/com/sunwook/wms/product/feature/RegisterProduct.java
@@ -1,8 +1,17 @@
 package com.sunwook.wms.product.feature;
 
 import com.sunwook.wms.product.domain.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RegisterProduct {
     private final ProductRepository productRepository;
 
@@ -11,24 +20,42 @@ public class RegisterProduct {
     }
 
 
-    public void request(final Request request) {
+    @PostMapping("/products")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void request(@RequestBody final Request request) {
         // request에서 필요한 값들을 사용해 상품 도메인을 생성하고 저장한다
         Product product = request.toDomain();
         productRepository.save(product);
     }
 
     public record Request(
+            @NotBlank(message = "상품명은 필수입니다")
             String name,
+            @NotBlank(message = "상품코드는 필수입니다")
             String code,
+            @NotBlank(message = "상품설명은 필수입니다")
             String description,
+            @NotBlank(message = "브랜드는 필수입니다")
             String brand,
+            @NotBlank(message = "제조사는 필수입니다")
             String maker,
+            @NotBlank(message = "원산지는 필수입니다")
             String origin,
+            @NotNull(message = "카테고리는 필수입니다")
             Category category,
+            @NotNull(message = "온도는 필수입니다")
             TemperatureZone temperatureZone,
+            @NotNull(message = "상품의 무게는 필수입니다")
+            @Min(value = 0, message = "무게는 0보다 작을 수 없습니다")
             Long weightInGrams,
+            @NotNull(message = "상품의 너비는 필수입니다")
+            @Min(value = 0, message = "너비는 0보다 작을 수 없습니다")
             Long widthInMillimeters,
+            @NotNull(message = "상품의 높이는 필수입니다")
+            @Min(value = 0, message = "높이는 0보다 작을 수 없습니다")
             Long heightInMillimeters,
+            @NotNull(message = "상품의 길이는 필수입니다")
+            @Min(value = 0, message = "길이는 0보다 작을 수 없습니다")
             Long lengthInMillimeters) {
 
         public Request {
--- src/test/java/com/sunwook/wms/product/feature/RegisterProductTest.java
+++ src/test/java/com/sunwook/wms/product/feature/RegisterProductTest.java
@@ -3,19 +3,30 @@ package com.sunwook.wms.product.feature;
 import com.sunwook.wms.product.domain.Category;
 import com.sunwook.wms.product.domain.ProductRepository;
 import com.sunwook.wms.product.domain.TemperatureZone;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RegisterProductTest {
+    @LocalServerPort
+    private int port;
 
     private RegisterProduct registerProduct;
     private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
+        if (RestAssured.UNDEFINED_PORT == RestAssured.port) {
+            RestAssured.port = port;
+        }
         productRepository = new ProductRepository();
         registerProduct = new RegisterProduct(productRepository);
     }
@@ -46,6 +57,14 @@ class RegisterProductTest {
         );
         //when
         registerProduct.request(request);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/products")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
 
         //then
         assertThat(productRepository.findAll()).hasSize(1);
