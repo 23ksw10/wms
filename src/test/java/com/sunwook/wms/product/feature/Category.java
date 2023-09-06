e754c64d03582bc760f3d2eb3b6685f0caa67657
Kim sun wook <kimsw9603@gmail.com>
2023-09-06 19:26:21 +0900

    5. move inner classes to upper level

---
+++ src/test/java/com/sunwook/wms/product/feature/Category.java
@@ -0,0 +1,12 @@
+package com.sunwook.wms.product.feature;
+
+public enum Category {
+
+    ELECTRONICS("전자기기");
+
+    private final String description;
+
+    Category(final String description) {
+        this.description = description;
+    }
+}
---
+++ src/test/java/com/sunwook/wms/product/feature/ProductSize.java
@@ -0,0 +1,30 @@
+package com.sunwook.wms.product.feature;
+
+import org.springframework.util.Assert;
+
+public class ProductSize {
+    private final Long widthInMillimeters;
+    private final Long heightInMillimeters;
+    private final Long lengthInMillimeters;
+
+    public ProductSize(
+            Long widthInMillimeters,
+            Long heightInMillimeters,
+            Long lengthInMillimeters) {
+        Assert.notNull(widthInMillimeters, "가로는 필수입니다");
+        if (0 > widthInMillimeters) {
+            throw new IllegalArgumentException("가로는 0보다 작을 수 없습니다");
+        }
+        Assert.notNull(heightInMillimeters, "높이는 필수입니다");
+        if (0 > heightInMillimeters) {
+            throw new IllegalArgumentException("높이는 0보다 작을 수 없습니다");
+        }
+        Assert.notNull(lengthInMillimeters, "길이는 필수입니다");
+        if (0 > lengthInMillimeters) {
+            throw new IllegalArgumentException("길이는 0보다 작을 수 없습니다");
+        }
+        this.widthInMillimeters = widthInMillimeters;
+        this.heightInMillimeters = heightInMillimeters;
+        this.lengthInMillimeters = lengthInMillimeters;
+    }
+}
